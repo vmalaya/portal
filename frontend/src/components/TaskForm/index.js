637f4ec1d4c93cd328af9117a08febbac47adbc6
Valentyna Melnychenko <valentinka.malaya@gmail.com>
2021-04-06 17:56:31 +0300

    Delete an old react integration. Restructure Layout integration for pages.

--- frontend/src/components/TaskForm/index.js
+++ frontend/src/components/TaskForm/index.js
@@ -6,30 +6,28 @@ const { Content } = Layout;
 
 const TaskForm = () => {
   return (
-    <Layout>
-      <Content style={{ 'padding': '20px', 'minHeight': '100vh' }}>
-        <PageHeader
-          backIcon={false}
-          className="site-page-header"
-          onBack={() => null}
-          title="Task"
-          subTitle=""
-        />
-        <Form>
-          <Space direction="vertical" style={{ 'width': '100%' }}>
-            <Input placeholder={'Name the task...'}/>
-            <TextArea autoSize={{ minRows: 12 }} placeholder={'Add a description...'}/>
-            <Row>
-              <Col span={8}/>
-              <Col span={8} style={{ 'display': 'flex', 'justifyContent': 'center' }}>
-                <Button type={'primary'}> Save Task </Button>
-              </Col>
-              <Col span={8}/>
-            </Row>
-          </Space>
-        </Form>
-      </Content>
-    </Layout>
+    <Content style={{ 'padding': '20px' }}>
+      <PageHeader
+        backIcon={false}
+        className="site-page-header"
+        onBack={() => null}
+        title="Task"
+        subTitle=""
+      />
+      <Form>
+        <Space direction="vertical" style={{ 'width': '100%' }}>
+          <Input placeholder={'Name the task...'}/>
+          <TextArea autoSize={{ minRows: 12 }} placeholder={'Add a description...'}/>
+          <Row>
+            <Col span={8}/>
+            <Col span={8} style={{ 'display': 'flex', 'justifyContent': 'center' }}>
+              <Button type={'primary'}> Save Task </Button>
+            </Col>
+            <Col span={8}/>
+          </Row>
+        </Space>
+      </Form>
+    </Content>
   );
 };
 
